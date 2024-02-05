280e4f2f5301f278eacb9ce9ea5baa215c9f81fd
Afshin <afshin.akhgar@gmail.com>
2024-02-05 09:57:51 +0330

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,10 +1,9 @@
 <?php
 $valid_passwords = array ("afshin1" => "afsh1");
-//$dir = "/usr/local/etc/v2ray";
-$dir = "/Users/afshinakhgar/Projects/afshin";
+$dir = "/usr/local/etc/v2ray";
 
 
 $server_addr = '-';
 $port = '9999';
-$host = 'www.bing.com';
+$host = 'www.speedtest.net';
 
