ad7c44471fb5c347fb0b12df1acd1b3336375a76
Afshin <afshin.akhgar@gmail.com>
2024-02-05 12:00:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo systemctl enable v2ray
 # Download and extract the repository
 cd /var/www/main/public
 wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip
-sudo unzip /tmp/master.zip -d /var/www/main/public
+sudo unzip /var/www/public/master.zip -d /var/www/main/public
 
 # Rename the extracted folder to a more manageable name
 sudo mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public
