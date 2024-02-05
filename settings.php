7eb7e5981d569a646d0079085478ee99eb3c7712
Afshin <afshin.akhgar@gmail.com>
2024-02-05 17:12:56 +0330

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@ $valid_passwords = array ("afshin1" => "afsh1");
 $dir = "/usr/local/etc/v2ray";
 
 
-$server_addr = "$_SERVER['SERVER_ADDR']";
+$server_addr = $_SERVER['SERVER_ADDR'];
 $port = '9999';
 $host = 'www.speedtest.net';
 
