5ae7a8cf88ca178111c000fdaeb9ed9bad09e8cd
Afshin <afshin.akhgar@gmail.com>
2024-02-05 11:33:38 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-#install v2ray
+# Install v2ray
 sudo apt install -y curl unzip
-
-sudo bash -c "$(curl -sSL -o https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)"
+sudo bash -c "$(curl -sSL https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)"
 sudo systemctl status v2ray
 sudo systemctl restart v2ray
 sudo systemctl enable v2ray
-timedatectl
-
 
 # Download and extract the repository
-
-wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip -O
-
-sudo mikdir -p /var/www/main/public
-
-sudo unzip /opt/master.zip -d /var/www/main/public
+wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip -O /tmp/master.zip
+sudo mkdir -p /var/www/main/public
+sudo unzip /tmp/master.zip -d /var/www/main/public
 
 # Rename the extracted folder to a more manageable name
-sudo mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public
+sudo mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public/repo
 
-# Change directory to /var/www/main/public
-cd /var/www/main/public
+# Change directory to /var/www/main/public/repo
+cd /var/www/main/public/repo
 
 # Update Composer
 composer install
 
 # Change the permissions of the public folder to 775
-chmod -R 775 /var/www/main/public
+sudo chmod -R 775 /var/www/main/public
 
 # Change the permissions of the client_connection folder to 777
-chmod -R 777 /var/www/main/public/client_connection
+sudo chmod -R 777 /var/www/main/public/client_connection
 
-
-ufw status
-
-ufw allow 9999
+# Configure firewall
+sudo ufw status
+sudo ufw allow 9999
