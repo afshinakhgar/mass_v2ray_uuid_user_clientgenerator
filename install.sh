a1708a573f551fe42a5b43dcf4bdfd6b46fd0b0c
Afshin <afshin.akhgar@gmail.com>
2024-02-05 10:57:29 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,23 +11,28 @@ timedatectl
 
 
 # Download and extract the repository
-wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip -O /tmp/repo.zip
-unzip /tmp/repo.zip -d /var/www/main/public
+wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip -O /opt/repo.zip
+unzip /opt/repo.zip -d /var/www/main/public
 
 
 
 
 # Rename the extracted folder to a more manageable name
-mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public/repo
+mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public
 
-# Change directory to /var/www/main/public/repo
-cd /var/www/main/public/repo
+# Change directory to /var/www/main/public
+cd /var/www/main/public
 
 # Update Composer
 composer install
 
 # Change the permissions of the public folder to 775
-chmod -R 775 /var/www/main/public/repo
+chmod -R 775 /var/www/main/public
 
 # Change the permissions of the client_connection folder to 777
-chmod -R 777 /var/www/main/public/repo/client_connection
+chmod -R 777 /var/www/main/public/client_connection
+
+
+ufw status
+
+ufw allow 9999
