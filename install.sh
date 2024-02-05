98ce535eb361817d63fb57fc2dc8145ffcefde56
Afshin <afshin.akhgar@gmail.com>
2024-02-05 10:25:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+#install v2ray
+sudo apt install -y curl unzip
+
+sudo bash -c "$(curl -sSL https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)"
+sudo systemctl status v2ray
+sudo systemctl restart v2ray
+sudo systemctl enable v2ray
+timedatectl
+
+
 # Download and extract the repository
 wget https://github.com/afshinakhgar/mass_v2ray_uuid_user_clientgenerator/archive/master.zip -O /tmp/repo.zip
 unzip /tmp/repo.zip -d /var/www/main/public
+
+
+
 
 # Rename the extracted folder to a more manageable name
 mv /var/www/main/public/mass_v2ray_uuid_user_clientgenerator-master /var/www/main/public/repo
